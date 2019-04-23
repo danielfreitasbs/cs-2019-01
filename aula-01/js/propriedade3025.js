96f076733dfad37d061dce11b8c53c4781f544e7
kyriosdata <fabio@inf.ufg.br>
2019-04-23 09:17:56 -0300

    metodo nao esta definido para valores fora da faixa (isso é diferente de retornar false)

--- aula-01/js/propriedade3025.js
+++ aula-01/js/propriedade3025.js
@@ -2,15 +2,16 @@ function propriedade3025(numero) {
     if (isNaN(numero)) {
         throw new TypeError("forneca um numero: " + numero);
     }
-    
-    if (numero >= 0 && numero <= 9999) {
-        const primeiraDezena = Math.floor(numero / 100);
-        const segundaDezena = numero % 100;
 
-        const soma = primeiraDezena + segundaDezena;
-        const quadradoDezenas = soma * soma;
-
-        return quadradoDezenas == numero;
+    if (numero < 0 || numero > 9999) {
+        throw new RangeError("numero fora da faixa: " + numero);
     }
-    return false;
+
+    const primeiraDezena = Math.floor(numero / 100);
+    const segundaDezena = numero % 100;
+
+    const soma = primeiraDezena + segundaDezena;
+    const quadradoDezenas = soma * soma;
+
+    return quadradoDezenas == numero;
 }
