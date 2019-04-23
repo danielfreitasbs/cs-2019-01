fa45deff312c1b1cf6c91fb4333338195878d809
kyriosdata <fabio@inf.ufg.br>
2019-04-23 09:07:17 -0300

    aproxime definicao e uso (alem de reduzir escopo)

--- aula-01/js/propriedade3025.js
+++ aula-01/js/propriedade3025.js
@@ -1,11 +1,11 @@
 function propriedade3025(numero) {
-    var primeiraDezena, segundaDezena, total;
+    var primeiraDezena, segundaDezena;
 
     if (numero >= 0 && numero <= 9999) {
         primeiraDezena = Math.floor(numero / 100);
         segundaDezena = numero % 100;
 
-        total = primeiraDezena + segundaDezena;
+        let total = primeiraDezena + segundaDezena;
         total = Math.pow(total, 2.0);
 
         return total == numero;
