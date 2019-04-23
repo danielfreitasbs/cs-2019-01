0225f36b803ac59c716645ca781646471a0d0d79
kyriosdata <fabio@inf.ufg.br>
2019-04-23 09:15:50 -0300

    metodo global (garbage in garbage out)

--- aula-01/js/propriedade3025.js
+++ aula-01/js/propriedade3025.js
@@ -1,4 +1,8 @@
 function propriedade3025(numero) {
+    if (isNaN(numero)) {
+        throw new TypeError("forneca um numero: " + numero);
+    }
+    
     if (numero >= 0 && numero <= 9999) {
         const primeiraDezena = Math.floor(numero / 100);
         const segundaDezena = numero % 100;
