e6aa19b36dcf5c82e0e6a9c039e78d4ad17087d1
Daniel Freitas <daniel@level5jr.com.br>
2019-05-30 21:39:51 -0300

    documentacao completa

--- aula-01/java/main/com/github/danielfreitasbs/exercicios/Propriedade3025.java
+++
@@ -1,9 +0,0 @@
-package com.github.danielfreitasbs.exercicios;
-
-public class Propriedade3025 {
-	public void verificaPropriedade3025(Integer inteiro) throws Exception {
-		if(inteiro.getClass() != Integer.class) {
-			throw new Exception("O valor informado deve ser um inteiro.");
-		}
-	}
-}
