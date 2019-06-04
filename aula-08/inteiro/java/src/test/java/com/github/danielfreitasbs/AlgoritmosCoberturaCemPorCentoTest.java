93b355b2e988b585b2c4a0307179bec87c96c444
Daniel Freitas <daniel.italofreitas@gmail.com>
2019-06-04 20:57:56 -0300

    criacao de arquivos

---
+++ aula-08/inteiro/java/src/test/java/com/github/danielfreitasbs/AlgoritmosCoberturaCemPorCentoTest.java
@@ -0,0 +1,12 @@
+package com.github.danielfreitasbs;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+public class AlgoritmosCoberturaCemPorCentoTest {
+    @Test
+    public void coberturaCemPorCento() {
+        assertNotNull(new Algoritmos());
+    }
+}
