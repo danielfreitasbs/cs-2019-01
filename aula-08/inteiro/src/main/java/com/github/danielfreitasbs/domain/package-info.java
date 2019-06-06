25db72786420fcea4bf0e3959fce42e877fc8503
Daniel Freitas <daniel@level5jr.com.br>
2019-06-06 20:44:53 -0300

    Criação do diretorio de leituro INTEIRO, e projeto do exercicio proposto 6 da aula 08

---
+++ aula-08/inteiro/src/main/java/com/github/danielfreitasbs/domain/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Pacote do repositório de "cs-2019-01/aula-08/inteiro". Contém duas classes
+ * apenas: (a) {@link com.github.danielfreitasbs.domain.LeituraArquivo} e
+ * (b) {@link com.github.danielfreitasbs.domain.application.Console}.
+ * Observe que essa última classe apenas ilustra como um
+ * programa em Java pode chamar o código da classe
+ * relevante ({@link com.github.danielfreitasbs.domain.LeituraArquivo}).
+ *
+ * @see com.github.danielfreitasbs.domain.LeituraArquivo
+ * @see com.github.danielfreitasbs.domain.application.Console
+ */
+
+package com.github.danielfreitasbs.domain;
