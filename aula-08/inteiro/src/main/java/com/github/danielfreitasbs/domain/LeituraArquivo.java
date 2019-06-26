78f5e9117dd72d00fafd14e25024f7b2bfd427cd
Daniel Freitas <danielgtavicy@gmail.com>
2019-06-25 21:19:06 -0300

    Revisao de testes aula 08 programa de inteiro

--- aula-08/inteiro/src/main/java/com/github/danielfreitasbs/domain/LeituraArquivo.java
+++ aula-08/inteiro/src/main/java/com/github/danielfreitasbs/domain/LeituraArquivo.java
@@ -1,6 +1,7 @@
 package com.github.danielfreitasbs.domain;
 
 import java.io.DataInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -17,34 +18,46 @@ public final class LeituraArquivo {
 	 * Atribuindo controle de acesso ao construtor da classe.
 	 */
 	private LeituraArquivo() {
-		
+
 	}
-	
+
 	/**
-	 * Realiza a leitura de um arquivo e retorna o seu nome em notação Hexadecimal. 
+	 * Realiza a leitura de um arquivo e retorna o seu nome em notação Hexadecimal.
 	 * 
 	 * @param arquivo nome do arquivo.
-	 * @throws FileNotFoundException Caso o arquivo não seja encontrado no diretorio, é disparada a exceção.
-	 * @throws IOException Caso ocorra algum erro na leitura do arquivo.
+	 * @return
+	 * @throws FileNotFoundException Caso o arquivo não seja encontrado no
+	 *                               diretorio, é disparada a exceção.
+	 * @throws IOException           Caso ocorra algum erro na leitura do arquivo.
 	 */
-	public static void lerArquivo(final String arquivo) throws FileNotFoundException, IOException {
-		if (arquivo == null) {
-			throw new IllegalArgumentException("Nome de arquivo invalido.");
-		} else {
-			try {
-				FileInputStream fis = new FileInputStream(arquivo);
-				DataInputStream dis = new DataInputStream(fis);
+	public static String lerArquivo(final String arquivo) throws FileNotFoundException, IOException {
+		validarParametros(arquivo);
+		
+		try {
+			FileInputStream fis = new FileInputStream(arquivo);
+			DataInputStream dis = new DataInputStream(fis);
 
-				final String nome = Integer.toHexString(dis.readInt());
-				dis.close();
-				
-				System.out.println(nome);
-			} catch (FileNotFoundException e) {
-				throw new FileNotFoundException("Arquivo não encontrado.");
-			} catch (IOException e) {
-				throw new IOException("Erro na leitura do arquivo.");
-			}
+			final String nome = Integer.toHexString(dis.readInt());
+			dis.close();
 
+			return nome;
+		} catch (FileNotFoundException e) {
+			throw new FileNotFoundException("Arquivo não encontrado.");
+		} catch (IOException e) {
+			throw new IOException("Erro na leitura do arquivo.");
+		}
+
+	}
+
+	static void validarParametros(final String caminhoArquivo) {
+
+		if (caminhoArquivo == null) {
+			throw new IllegalArgumentException("Arquivo não informado");
+		}
+
+		final File arquivo = new File(caminhoArquivo);
+		if (arquivo.length() == 0) {
+			throw new IllegalArgumentException("Arquivo vazio.");
 		}
 
 	}
--- aula-08/inteiro/src/test/java/com/github/danielfreitasbs/domain/LeituraArquivoTest.java
+++ aula-08/inteiro/src/test/java/com/github/danielfreitasbs/domain/LeituraArquivoTest.java
@@ -1,14 +1,39 @@
 package com.github.danielfreitasbs.domain;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
 
 public class LeituraArquivoTest {
+	
+    private String getFilename(String string) {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(string).getFile());
+
+        String absolutePath = file.getAbsolutePath();
+        return absolutePath;
+    }
+    
 	@Test
 	public void nomeValorNulo() {
 		assertThrows(IllegalArgumentException.class, () -> LeituraArquivo.lerArquivo(null));
 	}
+	
+	@Test
+	public void arquivoVazio() {
+		assertThrows(IllegalArgumentException.class, () -> LeituraArquivo.lerArquivo(getFilename("temNadaAqui.txt")));
+	}
+	
+	@Test
+	public void saidasCorretas() throws FileNotFoundException, IOException {
+		assertEquals("74657374616e646f", LeituraArquivo.lerArquivo(getFilename("74657374616e646f.txt")));
+		assertEquals("cafebabe", LeituraArquivo.lerArquivo(getFilename("cafebabe.class")));
+	}
+	
+	
 }
