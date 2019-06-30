94352c8bdce8d8a6892c9ba2fbb67ae3644fd8a2
Daniel Freitas <danielgtavicy@gmail.com>
2019-06-30 18:02:50 -0300

    Implementacao final revisao aula 8 exercicios inteiro

---
+++ aula-08/inteiro/src/main/java/com/github/danielfreitasbs/application/package-info.java
@@ -0,0 +1,4 @@
+/***
+ * Classe de execução do programa HexUtils.
+ */
+package com.github.danielfreitasbs.application;
--- aula-08/inteiro/src/main/java/com/github/danielfreitasbs/domain/package-info.java
+++ aula-08/inteiro/src/main/java/com/github/danielfreitasbs/domain/package-info.java
@@ -1,13 +1,12 @@
 /**
- * Pacote do repositório de "cs-2019-01/aula-08/inteiro". Contém duas classes
- * apenas: (a) {@link com.github.danielfreitasbs.domain.LeituraArquivo} e
- * (b) {@link com.github.danielfreitasbs.domain.application.Console}.
- * Observe que essa última classe apenas ilustra como um
- * programa em Java pode chamar o código da classe
- * relevante ({@link com.github.danielfreitasbs.domain.LeituraArquivo}).
+ * Pacote do repositório de "cs-2019-01/aula-08/inteiro". Contém duas classes apenas: (a)
+ * {@link com.github.danielfreitasbs.domain.HexUtils} e (b)
+ * {@link com.github.danielfreitasbs.application.Console}. Observe que essa última classe apenas
+ * ilustra como um programa em Java pode chamar o código da classe relevante
+ * ({@link com.github.danielfreitasbs.domain.HexUtils}).
  *
- * @see com.github.danielfreitasbs.domain.LeituraArquivo
- * @see com.github.danielfreitasbs.domain.application.Console
+ * @see com.github.danielfreitasbs.domain.HexUtils
+ * @see com.github.danielfreitasbs.application.Console
  */
 
 package com.github.danielfreitasbs.domain;
