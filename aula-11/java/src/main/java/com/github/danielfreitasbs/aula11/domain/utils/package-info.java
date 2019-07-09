681cd991b45889a0e6c924e1a0f7f8e3f057dca1
Daniel Freitas <danielgtavicy@gmail.com>
2019-07-09 20:20:24 -0300

    Implementação final de correçoes impostas pelo checkstyle e pmd.

--- aula-11/java/src/main/java/com/github/danielfreitasbs/aula11/domain/utils/package-info.java
+++ aula-11/java/src/main/java/com/github/danielfreitasbs/aula11/domain/utils/package-info.java
@@ -1,1 +1,7 @@
+/**
+ * Pacote responsavel por classes utilitarias para o formato Date. Até então composto pelas classes:
+ *
+ * @see com.github.danielfreitas.aula11.domain.utils.DateFormatUtils
+ * @see com.github.danielfreitas.aula11.domain.utils.DateMathUtils
+ */
 package com.github.danielfreitasbs.aula11.domain.utils;
--- aula-11/java/src/main/java/com/github/danielfreitasbs/aula11/domain/validations/DataValidationUtils.java
+++ aula-11/java/src/main/java/com/github/danielfreitasbs/aula11/domain/validations/DataValidationUtils.java
@@ -4,28 +4,28 @@ import static java.lang.Character.isDigit;
 
 /**
  * Classe realiza a validação de dados para o formato de data aqui especificados.
- * 
+ *
  * @author danielfreitasbs
  *
  */
-public class DataValidation {
+public final class DataValidationUtils {
 
   /**
    * Restrição de construtor.
    */
-  private DataValidation() {
+  private DataValidationUtils() {
 
   }
 
   /**
    * Realiza a validação de cada caracter constante na String. Caso seja nulo ou não seja um
    * numeral, ele irá disparar um erro.
-   * 
+   *
    * @param data String que contem a data recebida pela API.
    */
   public static void formatoDataValida(final String data) {
     dataNula(data);
-    for (Character caracter : data.toCharArray()) {
+    for (final Character caracter : data.toCharArray()) {
       if (!isDigit(caracter)) {
         throw new IllegalArgumentException("Valor atribuito para Data possui letras.");
       }
@@ -34,7 +34,7 @@ public class DataValidation {
 
   /**
    * Realiza a verificação se o parametro recebido pela API está nulo.
-   * 
+   *
    * @param data parametro a ser verificada a nulidade.
    */
   static void dataNula(final String data) {
